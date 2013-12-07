ce6630b3ae835445fd73765087aa9b2ae99b218f
Vic Wintriss <vic@wintrisstech.org>
2013-12-07 11:48:45 -0800

    first commit

--- src/ioio/examples/hello/MainActivity.java
+++ src/ioio/examples/hello/MainActivity.java
@@ -7,7 +7,6 @@ import ioio.lib.util.BaseIOIOLooper;
 import ioio.lib.util.IOIOLooper;
 import ioio.lib.util.android.IOIOActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.ToggleButton;
@@ -21,6 +20,7 @@ public class MainActivity extends IOIOActivity {
 	private ToggleButton button_;
 	private TextView mText;
 	private ScrollView scroller;
+	private int TotalCount = 488;//516 was 95
 
 	/**
 	 * Called when the activity is first created. Here we normally initialize
@@ -46,8 +46,8 @@ public class MainActivity extends IOIOActivity {
 		private DigitalOutput rightMotorClock;
 		private DigitalOutput leftMotorClock;
 		private DigitalOutput motorEn;
-		private PwmOutput rightMotorPWM;
-		private PwmOutput leftMotorPWM;
+//		private PwmOutput rightMotorPWM;
+//		private PwmOutput leftMotorPWM;
 		private DigitalOutput rightMotorDirection;
         private DigitalOutput leftMotorDirection;
 
@@ -62,21 +62,22 @@ public class MainActivity extends IOIOActivity {
 		 */
 		@Override
 		protected void setup() throws ConnectionLostException {
-			led_ = ioio_.openDigitalOutput(0, true);
-			// rightMotorClock = ioio_.openDigitalOutput(28);
-			// leftMotorClock = ioio_.openDigitalOutput(27);
-			motorEn = ioio_.openDigitalOutput(3);
+			led_ = ioio_.openDigitalOutput(0, true);             // LED pin
+			 rightMotorClock = ioio_.openDigitalOutput(28);
+			 leftMotorClock = ioio_.openDigitalOutput(27);
+			motorEn = ioio_.openDigitalOutput(3);                // motor enable pin
 			motorEn.write(true);
 			rightMotorClock.write(true);
 			leftMotorClock.write(true);
-			rightMotorPWM = ioio_.openPwmOutput(28, 200);//pin #, frequency right motor
-            leftMotorPWM = ioio_.openPwmOutput(27, 200);//pin #, frequency right motor
-            rightMotorPWM.setPulseWidth(10);
-            leftMotorPWM.setPulseWidth(10);
-            rightMotorDirection = ioio_.openDigitalOutput(20);
+//			rightMotorPWM = ioio_.openPwmOutput(28, 200);        //pin #, frequency right motor
+//            leftMotorPWM = ioio_.openPwmOutput(27, 200);         //pin #, frequency right motor
+//            rightMotorPWM.setPulseWidth(10);
+//            leftMotorPWM.setPulseWidth(10);
+            rightMotorDirection = ioio_.openDigitalOutput(20);   // Right Direction Pin
             rightMotorDirection.write(false);
-            leftMotorDirection = ioio_.openDigitalOutput(21);
+            leftMotorDirection = ioio_.openDigitalOutput(21);    // Left Direction Pin
             leftMotorDirection.write(true);
+            //log("hello");
 		}
 
 		/**
@@ -87,15 +88,18 @@ public class MainActivity extends IOIOActivity {
 		@Override
 		public void loop() throws ConnectionLostException {
 			led_.write(!button_.isChecked());
-			log("hello");
-//			try {
-//				rightMotorClock.write(true);
-//				rightMotorClock.write(false);
+			try {
+				rightMotorClock.write(true);
+				rightMotorClock.write(false);
 //				leftMotorClock.write(true);
 //				leftMotorClock.write(false);
-//				Thread.sleep(100);
-//			} catch (InterruptedException e) {
-//			}
+				Thread.sleep(50);
+				if (TotalCount-- < 0) {
+					motorEn.write(false);
+				}
+				
+			} catch (InterruptedException e) {
+			}
 		}
 	}
 
